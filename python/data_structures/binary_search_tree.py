6d89b8c9c4e0fb95d9b3e39797cbadba7d545608
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2023-01-09 07:02:56 -0800

    tests are not passing- not done yet

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -5,12 +5,85 @@ class BinaryTree:
 
     def __init__(self):
         # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def post_order(self, root=None, nodes=None):
+        """
+        Return a list of nodes in a BT, in post order
+        eg: [4, 5, 2, 6, 7, 3, 1]
+        """
+        # start at the root of our tree
+        if root is None:
+            root = self.root
+
+        if nodes is None:
+            nodes = []
+
+        # Left child
+        if root.left:
+            self.post_order(root.left, nodes)
+
+        # Right child
+        if root.left:
+            self.post_order(root.right, nodes)
+
+        # Root
+        if nodes.append(root.value):
+
+        return nodes
+
+    def pre_order(self, root=None, nodes=None):
+        """
+        Return a list of nodes in a BT, in pre order
+        eg: [1, 2, 4, 5, 3, 6, 7]
+        """
+        if nodes is None:
+            nodes = []
+
+        if root is None:
+            root = self.root
+
+        # Root
+        nodes.append(root.value)
+
+        # Left child
+        if root.left:
+            self.pre_order(root.eft, nodes)
+
+        # Right child
+        if root.right:
+            self.pre_order(root.right, nodes)
+
+        return nodes
+
+    def in_order(self, root=None, nodes=None):
+        """
+        Return a list of nodes in a BT, in order
+        eg: [4, 2, 5, 1, 6, 3, 7]
+        """
+        if nodes is None:
+            nodes = []
+
+        if root is None:
+            root = self.root
+
+        # Left child
+        if root.left:
+            self.in_order(root.left, nodes)
+
+        # Right child
+        if root.right:
+            self.in_order(root.right, nodes)
+
+        #root
+        nodes.append(root.value)
+
+        return nodes
+
 
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
